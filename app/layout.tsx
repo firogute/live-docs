6e2273659fcd2d5ce3eed0ee359245abf8427de6
Firomsa Guteta <firomsaguteta12@gmail.com>
2025-06-25 12:13:58 +0300

    Add authentication components and middleware with Clerk integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,8 +18,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={cn(`${inter.className} min-h-screen`)}>{children}</body>
-    </html>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+        variables: {
+          colorPrimary: "#3371FF",
+          fontSize: "16px",
+        },
+      }}
+    >
+      <html lang="en">
+        <body className={cn(`${inter.className} min-h-screen`)}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
