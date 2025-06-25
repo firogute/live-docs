3921fba170c712df074eab0a6d0ad82049f73c38
Firomsa Guteta <firomsaguteta12@gmail.com>
2025-06-25 12:02:29 +0300

    update dependencies and add new Header component
    - Updated various dependencies in package.json, including Clerk, Lexical, Liveblocks, Radix UI, and Sentry.
    - Removed the favicon.ico file from the public directory.
    - Added a new Header component to the components directory with responsive logo images.
    - Introduced TypeScript types in index.d.ts for better type safety and clarity in the codebase.

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,7 +1,20 @@
+import { Editor } from "@/components/editor/Editor";
+import Header from "@/components/Header";
 import React from "react";
 
 const Document = () => {
-  return <div>Document</div>;
+  return (
+    <>
+      <div>
+        <Header>
+          <div className="flex w-fit items-center justify-center gap-2">
+            <p className="document-title">Fake title</p>
+          </div>
+        </Header>
+        <Editor />
+      </div>
+    </>
+  );
 };
 
 export default Document;
