55f486af8d7b98efc9f582101b72460f66e39f86
Firomsa Guteta <firomsaguteta12@gmail.com>
2025-06-25 12:36:21 +0300

    add sign-in and sign-up pages with Clerk integration

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -15,7 +15,7 @@ const Document = () => {
       <div>
         <Header>
           <div className="flex w-fit items-center justify-center gap-2">
-            <p className="document-title">Fake title</p>
+            <p className="document-title">Share</p>
           </div>
 
           <SignedOut>
