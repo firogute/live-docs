2de001a0bbb621bee720319cf407d05c60984901
Firomsa Guteta <firomsaguteta12@gmail.com>
2025-06-26 15:10:16 +0300

    Add function to fetch and sort user data from Clerk API

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -7,13 +7,15 @@ import {
   ClientSideSuspense,
 } from "@liveblocks/react/suspense";
 import Loader from "@/components/Loader";
+import { getClerkUser } from "@/lib/actions/user.action";
 
 const Provider = ({ children }: { children: ReactNode }) => {
   return (
     <LiveblocksProvider
       authEndpoint="/api/liveblocks-auth"
       resolveUsers={async ({ userIds }) => {
-        // const users = await getClerkUsers()
+        const users = await getClerkUser({ userIds });
+        return users;
       }}
     >
       <ClientSideSuspense fallback={<Loader />}>{children}</ClientSideSuspense>
