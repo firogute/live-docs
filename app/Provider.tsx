7c74a8addcf29ca692fe4470f03384f4aaad1c70
Firomsa Guteta <firomsaguteta12@gmail.com>
2025-06-27 05:48:08 +0300

    add Comments component for threaded discussions in Editor

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -7,14 +7,14 @@ import {
   ClientSideSuspense,
 } from "@liveblocks/react/suspense";
 import Loader from "@/components/Loader";
-import { getClerkUser } from "@/lib/actions/user.action";
+import { getClerkUsers } from "@/lib/actions/user.action";
 
 const Provider = ({ children }: { children: ReactNode }) => {
   return (
     <LiveblocksProvider
       authEndpoint="/api/liveblocks-auth"
       resolveUsers={async ({ userIds }) => {
-        const users = await getClerkUser({ userIds });
+        const users = await getClerkUsers({ userIds });
         return users;
       }}
     >
